9ef7058578cfefafa21c94b909a1f24c7209e172
Saruj-Chy <saruj.chy222@gmail.com>
2020-06-07 11:24:11 +0600

    first commit

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState Called');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('deactivate called');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build called');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -12,7 +32,9 @@ class Screen2 extends StatelessWidget {
         child: RaisedButton(
           color: Colors.blue,
           child: Text('Go Back To Screen 1'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
     );
